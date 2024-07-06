634192e98eb41bba335ba00d8062231ac0b65011
tabshilla maureen <adongtm@gmail.com>
2024-07-06 15:15:06 +0300

    passed with flying colors

--- tdd.py
+++ tdd.py
@@ -3,3 +3,33 @@ def multiply(a,b):
     #product == 1
     return product
 multiply(1, 1)
+
+def multiply(a,b):
+    product = a* b
+    #product == 1
+    return product
+multiply(1, 1)
+
+def multiply(a,b):
+    product = a* b
+    #product == 4
+    return product
+multiply(2, 2)
+
+def multiply(a,b):
+    product = a* b
+    #product == 9
+    return product
+multiply(3, 3)
+
+def multiply(a,b):
+    product = a* b
+    #product == 16
+    return product
+multiply(4, 4)
+
+def multiply(a,b):
+    product = a* b
+    #product == 1035
+    return product
+multiply(23, 45)
